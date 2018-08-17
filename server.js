09a65d691b3527c1d587283e0e0bb524f4bef3ae
William Connelly <williamconnelly.dev@gmail.com>
2018-08-17 15:50:49 -0700

    Setup Index page, stylesheet, and script

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const bp = require("body-parser");
+const ejsLayouts = require("express-ejs-layouts");
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/static"));
+app.use(bp.urlencoded({extended: true}));
+app.use(ejsLayouts);
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.get('/resume', (req, res) => {
+	res.sendFile(__dirname+ '/static/resume/Scott_Ammon_Resume.pdf');
+})
+
+var server = app.listen(process.env.PORT || 3000);
+{console.log('Now Connected to Port: ' + port)}
+
+module.exports = server;
