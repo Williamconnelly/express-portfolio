13111146c811300c61b45c242c16704003d7548b
William Connelly <williamconnelly.dev@gmail.com>
2018-08-18 17:09:57 -0700

    Styling Project Page

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,40 @@
-console.log("Connected")
+// Add img src swapping when hovered
+var addGifHover = () => {
+  const previews = Array.from(document.getElementsByClassName("preview-image"));
+  previews.forEach(image => {
+    image.addEventListener("mouseover", () => {
+      switch(true) {
+        case(image.classList.contains("danmaku-img")):
+          image.src = "../img/danmaku.gif";
+          break;
+        case(image.classList.contains("movielist-img")):
+          image.src = "../img/movielist.gif"
+          break;
+        case(image.classList.contains("punchcard-img")):
+          image.src = "../img/punchcard.gif"
+          break;
+        default:
+          break;
+      }
+    })
+    image.addEventListener("mouseout", () => {
+      switch(true) {
+        case(image.classList.contains("danmaku-img")):
+          image.src = "../img/danmaku.png";
+          break;
+        case(image.classList.contains("movielist-img")):
+          image.src = "../img/movielist.png"
+          break;
+        case(image.classList.contains("punchcard-img")):
+          image.src = "../img/punchcard.png"
+          break;
+        default:
+          break;
+      }
+    });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", e => {
+  addGifHover();
+});
